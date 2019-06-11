89de3fd211cb31615cdb165913aa4798d96cde7f
chetuu16 <sanchisbeldajose@gmail.com>
2019-06-11 17:00:14 +0200

    colors changes

--- tiempo/src/Components/Header/Header.jsx
+++ tiempo/src/Components/Header/Header.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import './Header.css';
 
 const Header = () => {
+
+    if ("geolocation" in navigator) { //check geolocation available 
+        //try to get user current location using getCurrentPosition() method
+        navigator.geolocation.getCurrentPosition(function (position) {
+            console.log("Found your location nLat : " + position.coords.latitude + " nLang :" + position.coords.longitude);
+        });
+    } else {
+        console.log("Browser doesn't support geolocation!");
+    }
+    
     return (
         <header>
             <ul className="divMax">
